7625907d1a63c5cf6c5564f45fe48c3b04fea04c
Phillip Hoff <phillip@orst.edu>
2026-02-04 00:04:27 -0800

    Scaffold chart viewer app.

---
+++ src/EncDotNet.ChartViewer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace EncDotNet.ChartViewer.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ src/EncDotNet.ChartViewer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace EncDotNet.ChartViewer.ViewModels;
+
+public abstract class ViewModelBase : ReactiveObject
+{
+}
