c3b209ecc411a92c2c98079df5ab630f52a88ac5
Phillip Hoff <phillip@orst.edu>
2026-02-11 22:22:37 -0800

    Add borders for available maps.
    Signed-off-by: Phillip Hoff <phillip@orst.edu>

--- src/EncDotNet.ChartViewer/Views/MainWindow.axaml.cs
+++ src/EncDotNet.ChartViewer/Views/MainWindow.axaml.cs
@@ -1,10 +1,18 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Avalonia.Controls;
 using EncDotNet.ChartViewer.Catalogs;
+using EncDotNet.ChartViewer.Models;
 using EncDotNet.ChartViewer.ViewModels;
+using Mapsui;
+using Mapsui.Layers;
+using Mapsui.Nts;
+using Mapsui.Projections;
+using Mapsui.Styles;
 using Mapsui.Tiling;
+using NetTopologySuite.Geometries;
 
 namespace EncDotNet.ChartViewer.Views;
 
@@ -33,6 +41,10 @@ public partial class MainWindow : Window
         var chartIndexPath = Path.GetFullPath(Path.Combine(basePath, ChartIndexRelativePath));
         var catalogSource = new FileSystemCatalogSource(chartIndexPath);
         await vm.LoadCatalogAsync(catalogSource);
+
+        // Show chart boundaries on the map
+        var boundaryLayer = CreateChartBoundariesLayer(vm.AvailableCharts);
+        MyMapControl.Map?.Layers.Add(boundaryLayer);
 
         // Subscribe to chart selection changes
         foreach (var chartVm in vm.AvailableCharts)
@@ -121,6 +133,53 @@ public partial class MainWindow : Window
         }
 
         MyMapControl.Map?.Refresh();
+    }
+
+    private static MemoryLayer CreateChartBoundariesLayer(
+        IEnumerable<ChartViewModel> charts)
+    {
+        var features = new List<IFeature>();
+        var style = new VectorStyle
+        {
+            Fill = null,
+            Line = new Pen(Color.Red, 1),
+        };
+
+        foreach (var chartVm in charts)
+        {
+            var entry = chartVm.Entry;
+
+            if (entry.SouthLatitude is not { } south
+                || entry.NorthLatitude is not { } north
+                || entry.WestLongitude is not { } west
+                || entry.EastLongitude is not { } east)
+            {
+                continue;
+            }
+
+            var (minX, minY) = SphericalMercator.FromLonLat(west, south);
+            var (maxX, maxY) = SphericalMercator.FromLonLat(east, north);
+
+            var ring = new LinearRing(
+            [
+                new Coordinate(minX, minY),
+                new Coordinate(maxX, minY),
+                new Coordinate(maxX, maxY),
+                new Coordinate(minX, maxY),
+                new Coordinate(minX, minY),
+            ]);
+
+            var feature = new GeometryFeature(new Polygon(ring));
+            feature.Styles.Add(style);
+            features.Add(feature);
+        }
+
+        return new MemoryLayer
+        {
+            Name = "Chart Boundaries",
+            Features = features,
+            Style = null,
+        };
     }
 }
 
